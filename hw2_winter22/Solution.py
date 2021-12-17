1c2722966730257b14168f1195fdcf38d75fddf2
Jonathan Josef <jjosef@microsoft.com>
2021-12-17 20:45:13 +0200

    Handled all exception mechanism for implemented functions. Implement first functions of Basic API

--- hw2_winter22/Solution.py
+++ hw2_winter22/Solution.py
@@ -17,16 +17,22 @@ Views = []
 
 
 def _errorHandling(e) -> ReturnValue:
+    if isinstance(e, DatabaseException.NOT_NULL_VIOLATION) or \
+            isinstance(e, DatabaseException.CHECK_VIOLATION):
+        return ReturnValue.BAD_PARAMS
+
     if isinstance(e, DatabaseException.UNIQUE_VIOLATION):
         return ReturnValue.ALREADY_EXISTS
 
     if isinstance(e, DatabaseException.FOREIGN_KEY_VIOLATION):
         return ReturnValue.BAD_PARAMS
 
-    if isinstance(e, DatabaseException.database_ini_ERROR):
-        return ReturnValue.NOT_EXISTS
+    if isinstance(e, DatabaseException.database_ini_ERROR) or \
+            isinstance(e, DatabaseException.ConnectionInvalid):
+        return ReturnValue.ERROR
 
     return ReturnValue.OK
+    # return ReturnValue.UNKNOWN_ERROR
 
 
 def sendQuery(query) -> collections.namedtuple("QueryResult", ["Status", "Set"]):
@@ -37,16 +43,35 @@ def sendQuery(query) -> collections.namedtuple("QueryResult", ["Status", "Set"])
         res = dbConnector.execute(query=query)
     except BaseException as e:
         retValue = _errorHandling(e)
-
-    dbConnector.close()
+    finally:
+        dbConnector.close()
 
     queryResult = collections.namedtuple("QueryResult", ["Status", "Set"])
     return queryResult(retValue, res)
 
 
-def _createTable(name, colNames, colTypes, extraProperties, foreignKey = None, references = None):
+def _createTable(name, colNames, colTypes, extraProperties, foreignKey = None, references = None, explicitPrimaryKey = None):
+    """
+    :param name: The Table name
+    :param colNames: a list of column names
+    :param colTypes: a list of column types
+    :param extraProperties: Extra properties per parameter. e.g: UNIQUE, PRIMARY KEY, NOT NULL, etc.
+    :param foreignKey: a list of col names (each name must be contained in 'colNames'), which are a foreign keys
+    :param references: a list of strings represent the references of the foreign keys. Should have a format of 'Table(relevantCol)'.
+    :param explicitPrimaryKey: Sometimes, we'll want to set new col explicitly for a Primary key (mainly if the key consists multiple cols)
+            Should be a tuple of ('newColName', 'col1, col2, col3')
+    :return: a dictionary with the table metadata for the table generator
+    """
+    if foreignKey is None:
+        foreignKey = []
+    if references is None:
+        references = []
+    if explicitPrimaryKey is None:
+        explicitPrimaryKey = []
+
     assert len(colNames) == len(colTypes)
     assert len(colNames) == len(extraProperties)
+    assert len(foreignKey) == len(references)
 
     return {
         "name": name,
@@ -54,10 +79,13 @@ def _createTable(name, colNames, colTypes, extraProperties, foreignKey = None, r
         "colTypes": colTypes,
         "extraProperties": extraProperties,
         "foreignKey": foreignKey,
-        "references": references
+        "references": references,
+        "explicitPrimaryKey": explicitPrimaryKey
     }
 
+# endregion
 
+# region Init
 def defineTables():
     table_Teams = _createTable(name="Teams",
                                colNames=["teamId"],
@@ -67,35 +95,41 @@ def defineTables():
     table_Players = _createTable(name="Players",
                                  colNames=["playerId", "teamId", "age", "height", "preferredFoot"],
                                  colTypes=["int", "int", "int", "int", "varchar(255)"],
-                                 extraProperties=["PRIMARY KEY", "NOT NULL", "NOT NULL", "NOT NULL", "NOT NULL"])
-    table_Scores = _createTable(name="Scores",
-                                colNames=["playerId", "matchId", "amount"],
-                                colTypes=["int", "int", "int"],
-                                extraProperties=["UNIQUE NOT NULL", "NOT NULL", "NOT NULL"])
+                                 extraProperties=["PRIMARY KEY", "NOT NULL", "NOT NULL", "NOT NULL", "NOT NULL"],
+                                 foreignKey=["teamId"],
+                                 references=["Teams(teamId)"])
 
     table_Matches = _createTable(name="Matches",
                                  colNames=["matchId", "competition", "homeTeamId", "awayTeamId"],
                                  colTypes=["int", "varchar(255)", "int", "int"],
-                                 extraProperties=["UNIQUE NOT NULL", "NOT NULL", "NOT NULL", "NOT NULL"])
+                                 extraProperties=["PRIMARY KEY", "NOT NULL", "NOT NULL", "NOT NULL"])
+
+    table_Stadiums = _createTable(name="Stadiums",
+                                  colNames=["stadiumId", "capacity", "teamId"],
+                                  colTypes=["int", "int", "int"],
+                                  extraProperties=["PRIMARY KEY", "NOT NULL", "UNIQUE"],
+                                  foreignKey=["teamId"],
+                                  references=["Teams(teamId)"])
+
+    table_Scores = _createTable(name="Scores",
+                                colNames=["playerId", "matchId", "amount"],
+                                colTypes=["int", "int", "int"],
+                                extraProperties=["NOT NULL", "NOT NULL", "NOT NULL"],
+                                foreignKey=["playerId", "matchId"],
+                                references=["Players(playerId)", "Matches(matchId)"],
+                                explicitPrimaryKey=[("match_player", "playerId, matchId")])
 
     table_MatchInStadium = _createTable(name="MatchInStadium",
                                         colNames=["matchId", "stadiumId", "attendance"],
                                         colTypes=["int", "int", "int"],
                                         extraProperties=["UNIQUE NOT NULL", "NOT NULL", "NOT NULL"])
 
-    table_Stadiums = _createTable(name="Stadiums",
-                                  colNames=["stadiumId", "capacity", "teamId"],
-                                  colTypes=["int", "int", "int"],
-                                  extraProperties=["UNIQUE NOT NULL", "NOT NULL", ""],
-                                  foreignKey="teamId",
-                                  references="Teams(teamId)")
-
     Tables.append(table_Teams)
     Tables.append(table_Players)
-    Tables.append(table_Scores)
     Tables.append(table_Matches)
-    Tables.append(table_MatchInStadium)
     Tables.append(table_Stadiums)
+    Tables.append(table_Scores)
+    Tables.append(table_MatchInStadium)
 
 
 def defineViews():
@@ -106,9 +140,7 @@ def defineViews():
     }
     Views.append(view_ActiveTallTeams)
 
-# endregion
 
-# region Init
 def createTables():
 
     defineTables()
@@ -117,20 +149,29 @@ def createTables():
     # Table creator generator
     for table in Tables:
         q = "CREATE TABLE " + table["name"] + " ("
+
+        # add cols
         for col_index in range(len(table["colNames"])):
             q += table["colNames"][col_index] + " " + table["colTypes"][col_index]
             q += " " + table["extraProperties"][col_index]
             if col_index < len(table["colNames"]) - 1:
                 q += ", "
 
-        if table["foreignKey"]:
-            q += ", FOREIGN KEY ("+table["foreignKey"]+") REFERENCES "+table["references"]
+        # add foreign keys if exists
+        for key, ref in zip(table["foreignKey"], table["references"]):
+            q += ", FOREIGN KEY ("+key+") REFERENCES "+ref
+
+        # add special primary keys if exists
+        for newCol, oldCols in table["explicitPrimaryKey"]:
+            q += ", CONSTRAINT " + newCol + " PRIMARY KEY (" + oldCols + ")"
+
         q += ")"
 
         sendQuery(q)
 
     for view in Views:
         pass  # TODO : Jonathan
+
 
 def clearTables():
     for table in Tables:
@@ -148,8 +189,10 @@ def dropTables():
 # region Team
 
 def addTeam(teamID: int) -> ReturnValue:
-    q = "INSERT INTO Teams (teamId) VALUES (" + str(teamID) + ");"
+    if teamID <= 0:
+        return ReturnValue.BAD_PARAMS
 
+    q = "INSERT INTO Teams (teamId) VALUES (" + str(teamID) + ");"
     return sendQuery(q).Status
 
 # endregion
@@ -167,15 +210,20 @@ def _sqlToMatch(res: Connector.ResultSet) -> Match:
 
 def addMatch(match: Match) -> ReturnValue:
     matchId = match.getMatchID()
-    competiton = match.getCompetition()
+    competition = match.getCompetition()
     homeTeam = match.getHomeTeamID()
     awayTeam = match.getAwayTeamID()
 
+    if None in [matchId, competition, homeTeam, awayTeam]:
+        return ReturnValue.BAD_PARAMS
+
+    if matchId <= 0 or competition not in ["International", "Domestic"] or homeTeam == awayTeam:
+        return ReturnValue.BAD_PARAMS
+
     q = "INSERT INTO Matches (matchId ,competition, homeTeamId, awayTeamId) VALUES ("
-    q += str(matchId) + " , '" + competiton + "' , " + str(homeTeam) + " , " + str(awayTeam) + " );"
+    q += str(matchId) + " , '" + competition + "' , " + str(homeTeam) + " , " + str(awayTeam) + " );"
 
-    return sendQuery(q).Status
-
+    return sendQuery(q).Status  # TODO: check non-positive/non-existence teams and look for "Bad Params"
 
 
 def getMatchProfile(matchID: int) -> Match:
@@ -206,12 +254,24 @@ def _sqlToPlayer(res: Connector.ResultSet) -> Player:
 
 
 def addPlayer(player: Player) -> ReturnValue:
+    playerId = player.getPlayerID()
+    teamId = player.getTeamID()
+    age = player.getAge()
+    height = player.getHeight()
+    foot = player.getFoot()
+
+    if None in [playerId, teamId, age, height, foot]:
+        return ReturnValue.BAD_PARAMS
+
+    if min(playerId, teamId, age, height) <= 0 or foot not in ["Left", "Right"]:
+        return ReturnValue.BAD_PARAMS
+
     q = "INSERT INTO players (playerId, teamId, age, height, preferredFoot) VALUES ("
-    q += str(player.getPlayerID()) + ", "
-    q += str(player.getTeamID()) + ", "
-    q += str(player.getAge()) + ", "
-    q += str(player.getHeight()) + ", "
-    q += "'" + str(player.getFoot()) + "');"
+    q += str(playerId) + ", "
+    q += str(teamId) + ", "
+    q += str(age) + ", "
+    q += str(height) + ", "
+    q += "'" + str(foot) + "');"
     return sendQuery(q).Status
 
 
@@ -238,17 +298,24 @@ def _sqlToStadium(res: Connector.ResultSet) -> Stadium:
                    capacity=row[1],
                    belongsTo=row[2])    #TODO: check if ok with Null
 
+
 def addStadium(stadium: Stadium) -> ReturnValue:
-    belongToIsNotNone = stadium.getBelongsTo() != None
+    # TODO: check return ALREADY_EXISTS if a Stadium with the same ID already exists or the team already owns a stadium
+    stadiumId = stadium.getStadiumID()
+    cap = stadium.getCapacity()
+    belong = stadium.getBelongsTo()
+
+    if None in [stadiumId, cap] or min(stadiumId, cap) <= 0 or (belong and belong <= 0):    # Last one is a bit tricky, it checks if 'belong' is valid only of it isn't None
+        return ReturnValue.BAD_PARAMS
 
     q = "INSERT INTO stadiums (stadiumId, capacity"
-    if belongToIsNotNone:
+    if belong:
         q += ", teamId"
     q += ") VALUES ("
-    q += str(stadium.getStadiumID())
-    q += ", " + str(stadium.getCapacity())
-    if belongToIsNotNone:
-        q += ", " + str(stadium.getBelongsTo())
+    q += str(stadiumId)
+    q += ", " + str(cap)
+    if belong:
+        q += ", " + str(belong)
     q += ")"
 
     return sendQuery(q).Status
@@ -272,8 +339,14 @@ def deleteStadium(stadium: Stadium) -> ReturnValue:
 
 # region Basic API
 def playerScoredInMatch(match: Match, player: Player, amount: int) -> ReturnValue:
-    pass
+    if amount <= 0:
+        return ReturnValue.BAD_PARAMS
 
+    q = "INSERT INTO ScoresScores (playerId, matchId, amount) VALUES ("
+    q += str(player.getPlayerID()) + ", "
+    q += str(match.getMatchID()) + ", "
+    q += str(amount) + ");"
+    return sendQuery(q).Status
 
 def playerDidntScoreInMatch(match: Match, player: Player) -> ReturnValue:
     pass
