9a025bf51f287b24913c47c02be29e51bac4919f
tayaharshuk <57790114+tayaharshuk@users.noreply.github.com>
2021-12-22 16:22:11 +0200

    fc

--- hw2_winter22/Tests/SimpleTest.py
+++ hw2_winter22/Tests/SimpleTest.py
@@ -13,23 +13,57 @@ from Business.Player import Player
 
 
 class Test(AbstractTest):
+
+    def _comparePlayer(self, p1:Player, p2:Player):
+        return (p1.getPlayerID() == p2.getPlayerID()
+                and p1.getFoot()==p2.getFoot()
+                and p1.getAge()==p2.getAge()
+                and p1.getHeight()==p2.getHeight()
+                and p1.getTeamID()==p2.getTeamID())
+
+    def _compareMatch(self, m1:Match, m2:Match):
+        return(m1.getMatchID()==m2.getMatchID()
+               and m1.getAwayTeamID()==m2.getAwayTeamID()
+               and m1.getHomeTeamID()==m2.getHomeTeamID()
+               and m1.getCompetition()==m2.getCompetition())
+
+    def _compareStadium(self, s1:Stadium , s2:Stadium):
+        return (s1.getBelongsTo()==s2.getBelongsTo()
+                and s1.getCapacity()==s2.getCapacity()
+                and s1.getStadiumID()== s2.getStadiumID())
+
+
     def test_Team(self) -> None:
         self.assertEqual(ReturnValue.OK, Solution.addTeam(1), "Should work")
         self.assertEqual(ReturnValue.OK, Solution.addTeam(2), "Should work")
         self.assertEqual(ReturnValue.ALREADY_EXISTS, Solution.addTeam(1), "ID 1 already exists")
+        self.assertEqual(ReturnValue.ERROR, Solution.addTeam("t"), "Should work") #TODO: is it really supposed to be error? not BADPARAMS?
+
 
     def test_Match(self) -> None:
+        #fails when trying to return a matchProfile, maybe i wrote it in a wrong way because it really returns a Match
+        # but not the one i wrote in the assert
+
         self.assertEqual(ReturnValue.OK, Solution.addTeam(1), "Should work")
         self.assertEqual(ReturnValue.OK, Solution.addTeam(2), "Should work")
         self.assertEqual(ReturnValue.OK, Solution.addTeam(3), "Should work")
         self.assertEqual(ReturnValue.OK, Solution.addTeam(4), "Should work")
         self.assertEqual(ReturnValue.OK, Solution.addTeam(5), "Should work")
+        self.assertEqual(ReturnValue.OK, Solution.addStadium(Stadium(10,50,1)), "Should work")
         self.assertEqual(ReturnValue.OK, Solution.addMatch(Match(1, "Domestic", 1, 2)), "Should work")
+        self.assertEqual(ReturnValue.OK, Solution.matchInStadium(Match(1, "Domestic", 1, 2),Stadium(10,50,1),50),"Should work")
         self.assertEqual(ReturnValue.OK, Solution.addMatch(Match(2, "Domestic", 1, 3)), "Should work")
         self.assertEqual(ReturnValue.OK, Solution.addMatch(Match(3, "Domestic", 1, 4)), "Should work")
+        self.assertEqual(True, self._compareMatch(Match(2, "Domestic", 1, 3), Solution.getMatchProfile(2)))
         self.assertEqual(ReturnValue.ALREADY_EXISTS, Solution.addMatch(Match(1, "Domestic", 1, 5)), "ID 1 already exists")
-        self.assertEqual(ReturnValue.OK, Solution.deleteMatch(Match(1)), "Should work")
-        self.assertEqual(ReturnValue.NOT_EXISTS, Solution.deleteMatch(Match(1)), "Match already deleted")
+        self.assertEqual(ReturnValue.OK, Solution.deleteMatch(Match(1, "Domestic", 1, 2)), "Should work")
+        self.assertEqual(ReturnValue.NOT_EXISTS, Solution.deleteMatch(Match(1, "Domestic", 1, 2)), "Match already deleted")
+        self.assertEqual(ReturnValue.NOT_EXISTS, Solution.matchNotInStadium(Match(1, "Domestic", 1, 2),Stadium(10)), "Should work")
+        self.assertEqual(ReturnValue.OK, Solution.addMatch(Match(1, "Domestic", 1, 2)), "Should work")
+        self.assertEqual(True, self._compareMatch(Match(1, "Domestic", 1, 2), Solution.getMatchProfile(1)))
+        self.assertEqual(ReturnValue.OK, Solution.deleteMatch(Match(1, "Domestic", 1, 2)), "Should work")
+        self.assertEqual(True, self._compareMatch(Match.badMatch(), Solution.getMatchProfile(1)))
+
 
     def test_Player(self) -> None:
         self.assertEqual(ReturnValue.OK, Solution.addTeam(1), "Should work")
@@ -37,6 +71,10 @@ class Test(AbstractTest):
         self.assertEqual(ReturnValue.OK, Solution.addPlayer(Player(2, 1, 20, 185, "Left")), "Should work")
         self.assertEqual(ReturnValue.OK, Solution.addPlayer(Player(3, 1, 20, 185, "Left")), "Should work")
         self.assertEqual(ReturnValue.ALREADY_EXISTS, Solution.addPlayer(Player(1, 1, 20, 185, "Left")), "ID 1 already exists")
+        self.assertEqual(ReturnValue.OK, Solution.deletePlayer(Player(3, 1, 20, 185, "Left")), "Should work")
+        self.assertEqual(True, self._comparePlayer(Solution.getPlayerProfile(3), Player.badPlayer()), "should work") #TODO
+        self.assertEqual(ReturnValue.OK, Solution.addPlayer(Player(3, 1, 20, 185, "Left")), "Should work")
+        self.assertEqual(True, self._comparePlayer(Player(3, 1, 20, 185, "Left"),Solution.getPlayerProfile(3)))  #?? TODO
 
     def test_Stadium(self) -> None:
         self.assertEqual(ReturnValue.OK, Solution.addTeam(1), "Should work")
@@ -45,6 +83,14 @@ class Test(AbstractTest):
         self.assertEqual(ReturnValue.OK, Solution.addStadium(Stadium(1, 55000, 1)), "Should work")
         self.assertEqual(ReturnValue.ALREADY_EXISTS, Solution.addStadium(Stadium(1, 5000, 1)), "ID 1 already exists")
         self.assertEqual(ReturnValue.BAD_PARAMS, Solution.addStadium(Stadium(2, 5000, 3)), "teamID 3 not exists")
+        self.assertEqual(True, self._compareStadium(Stadium(1, 55000, 1),Solution.getStadiumProfile(1)))
+        self.assertEqual(True, self._compareStadium(Stadium.badStadium(),Solution.getStadiumProfile(2)))
+        self.assertEqual(ReturnValue.OK, Solution.deleteStadium(Stadium(1,55000,1)))
+        self.assertEqual(True, self._compareStadium(Stadium.badStadium(), Solution.getStadiumProfile(1)))
+        self.assertEqual(ReturnValue.OK, Solution.addStadium(Stadium(1, 55000, 1)), "Should work")
+        self.assertEqual(True,self._compareStadium(Stadium(1, 55000, 1), Solution.getStadiumProfile(1)))
+        self.assertEqual(ReturnValue.NOT_EXISTS, Solution.deleteStadium(Stadium(2, 55000, 1)))
+
 
     def test_MatchBetweenSameTeam(self) -> None:
         self.assertEqual(ReturnValue.OK, Solution.addTeam(1), "Should work")
@@ -63,12 +109,14 @@ class Test(AbstractTest):
         self.assertEqual(ReturnValue.BAD_PARAMS, Solution.addMatch(Match(-1, "Domestic", 1, 2)), "Non Positive IDs")
         self.assertEqual(ReturnValue.BAD_PARAMS, Solution.addStadium(Stadium(-1, 5000, 1)), "Non Positive IDs")
         self.assertEqual(ReturnValue.BAD_PARAMS, Solution.addStadium(Stadium(1, -100, 1)), "Non Positive IDs")
+        self.assertEqual(ReturnValue.BAD_PARAMS, Solution.addStadium(Stadium(1, 100, -1)), "Non Positive IDs")
 
     def test_strings(self) -> None:
         self.assertEqual(ReturnValue.OK, Solution.addTeam(1), "Should work")
         self.assertEqual(ReturnValue.BAD_PARAMS, Solution.addPlayer(Player(1, 1, 20, 185, "Lefti like it")), "Bad String")
         self.assertEqual(ReturnValue.BAD_PARAMS, Solution.addMatch(Match(1, "Dome", 1, 2)), "Bad String")
         self.assertEqual(ReturnValue.BAD_PARAMS, Solution.addMatch(Match(1, "domestic", 1, 2)), "Bad String")
+        self.assertEqual(ReturnValue.BAD_PARAMS, Solution.addMatch(Match(1, "DOMESTIC", 1, 2)), "Bad String")
 
     def test_PlayerScore(self):
         self.assertEqual(ReturnValue.OK, Solution.addTeam(1), "Should work")
@@ -83,6 +131,7 @@ class Test(AbstractTest):
         self.assertEqual(ReturnValue.BAD_PARAMS, Solution.playerScoredInMatch(Match(matchID=1), Player(playerID=1), 0), "amount not positive")
         self.assertEqual(ReturnValue.BAD_PARAMS, Solution.playerScoredInMatch(Match(matchID=1), Player(playerID=1), -1), "amount not positive")
         self.assertEqual(ReturnValue.OK, Solution.playerScoredInMatch(Match(matchID=1), Player(playerID=1), 1), "Should work")
+        self.assertEqual(ReturnValue.ALREADY_EXISTS, Solution.playerScoredInMatch(Match(1),Player(1),1), "already Scored")
 
     def test_playerIsWinner(self):
         self.assertEqual(ReturnValue.OK, Solution.addTeam(1), "Should work")
@@ -297,39 +346,53 @@ class Test(AbstractTest):
         self.assertEqual(ReturnValue.OK, Solution.playerScoredInMatch(Match(matchID=7), Player(playerID=7), 1), "Should work")
         self.assertEqual([3, 6], Solution.getClosePlayers(1))
         self.assertEqual([1, 2, 3, 4, 5, 6, 7, 9, 10, 11], Solution.getClosePlayers(8))
+        self.assertEqual(ReturnValue.OK, Solution.deleteMatch(Match(3)))
+        self.assertEqual([6], Solution.getClosePlayers(1))
 
 
-        def test_deleteOfRecord:
+    def test_deleteOfRecord(self):
         #teams: 1,2,3
         #player 1 in team 2
 
         #stadiums: 10,20
 
         #return
-        #TODO: shouldwork? , when we have BADPARAMS?
+        #TODO: 339 and 345
 
-        self.assertEqual((ReturnValue.OK,Solution.addTeam(1)), "Should work")
-        self.assertEqual((ReturnValue.OK, Solution.addTeam(2)), "Should work")
-        self.assertEqual((ReturnValue.OK, Solution.addTeam(30)), "Should work")
-        self.assertEqual((ReturnValue.ALREADY_EXISTS, Solution.addTeam(1)), "Shoult work")  #trying to add again team1
-        #self.assertEqual((ReturnValue.OK, Solution.defineTables),)
-        self.assertEqual(ReturnValue.OK,Solution.addPlayer(Player(1,2,20,195, "Left")))  #add player 1 to team 2
+        self.assertEqual(ReturnValue.OK, Solution.addTeam(1), "Should work")
+        self.assertEqual(ReturnValue.OK, Solution.addTeam(2), "Should work")
+        self.assertEqual(ReturnValue.OK, Solution.addTeam(30), "Should work")
+        self.assertEqual(ReturnValue.ALREADY_EXISTS, Solution.addTeam(1), "Shoult work")  #trying to add again team1
+        self.assertEqual(ReturnValue.OK, Solution.addPlayer(Player(1,2,20,195, "Left")))  #add player 1 to team 2
         self.assertEqual(ReturnValue.ALREADY_EXISTS, Solution.addPlayer(Player(1, 2, 20, 195, "Left"))) #trying to add player 1 to team 2
-        self.assertEqual(ReturnValue.ALREADY_EXISTS, Solution.addPlayer(Player(1, 3, 20, 195, "Left")))
+        self.assertEqual(ReturnValue.OK, Solution.addPlayer(Player(2, 1, 20, 195, "Left")))
         self.assertEqual(ReturnValue.OK, Solution.deletePlayer(Player(1, 2, 20, 195, "Left")))  #deleting player 1
-        self.assertEqual(ReturnValue.OK, Solution.getPlayerProfile(1))     #trying to get profile of a player who doesnt exist
-        self.assertEqual((ReturnValue.OK, Solution.addStadium(Stadium(10,300))),"Should work")
-        self.assertEqual((ReturnValue.OK, Solution.addStadium(Stadium(20, 200))),"Should work")
-        self.assertEqual((ReturnValue.ALREADY_EXISTS, Solution.addStadium(Stadium(10, 300))), "Should work") #trying to add an existing stadium
-        self.assertEqual((ReturnValue.OK, Solution.deleteStadium(Stadium(20, 200))),"Should work" )
-        self.assertEqual((ReturnValue.NOT_EXISTS, Solution.getStadiumProfile(20),"Should work") #trying to get a profile of stadium that doesnt exist
+        self.assertEqual(True, self._comparePlayer(Player.badPlayer(), Solution.getPlayerProfile(1)))     #trying to get profile of a player who doesnt exist
+        self.assertEqual(ReturnValue.OK, Solution.addStadium(Stadium(10,300)),"Should work")
+        self.assertEqual(ReturnValue.OK, Solution.addStadium(Stadium(20, 200)),"Should work")
+        self.assertEqual(ReturnValue.ALREADY_EXISTS, Solution.addStadium(Stadium(10, 300)), "Should work") #trying to add an existing stadium
+        self.assertEqual(ReturnValue.OK, Solution.deleteStadium(Stadium(20, 200)),"Should work" )
+        self.assertEqual(True,self._compareStadium(Stadium.badStadium(), Solution.getStadiumProfile(20))) #trying to get a profile of stadium that doesnt exist
 
 
 
+    def test_cleanTables(self):
+        self.assertEqual(ReturnValue.OK, Solution.addTeam(1), "Should work")
+        self.assertEqual(ReturnValue.OK, Solution.addPlayer(Player(1, 1, 20, 195, "Left")), "Should work")
+        self.assertEqual(ReturnValue.OK, Solution.addStadium(Stadium(1,55000,1)),"Should work")
+        self.assertEqual(ReturnValue.OK, Solution.addMatch(Match(1, "Domestic", 1, 2)),"Should work")
+        Solution.clearTables()
+        self.assertEqual(True, self._comparePlayer(Player.badPlayer(), Solution.getPlayerProfile(1)), "Should work")
+        self.assertEqual(True,self._compareMatch(Match.badMatch(), Solution.getMatchProfile(2)), "Should work")
+        self.assertEqual(True,self._compareStadium(Stadium.badStadium(), Solution.getStadiumProfile(1)), "Should work")
+        self.assertEqual(ReturnValue.BAD_PARAMS, Solution.addPlayer(Player(1, 1, 20, 195, "Left")), "Should work")
+        self.assertEqual(ReturnValue.OK, Solution.addTeam(1), "Should work")
+        self.assertEqual(ReturnValue.OK, Solution.addPlayer(Player(1, 1, 20, 195, "Left")), "Should work")
+        self.assertEqual(True, self._comparePlayer(Player(1, 1, 20, 195, "Left"), Solution.getPlayerProfile(1)), "Should work")
+        #self.assertEqual(ReturnValue.OK, Solution.getPlayerProfile(1), "Should work")
 
-        def test_getRecords:
 
-        def test_cleanTables
+
 
 # *** DO NOT RUN EACH TEST MANUALLY ***
 if __name__ == '__main__':
